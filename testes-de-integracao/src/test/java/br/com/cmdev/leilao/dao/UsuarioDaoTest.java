a7a2a01e43d17fb5831599a954fed77c2eb4e217
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-26 06:43:38 -0300

    03.Organização do código de teste

--- testes-de-integracao/src/test/java/br/com/cmdev/leilao/dao/UsuarioDaoTest.java
+++ testes-de-integracao/src/test/java/br/com/cmdev/leilao/dao/UsuarioDaoTest.java
@@ -1,9 +1,12 @@
 package br.com.cmdev.leilao.dao;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,26 +16,37 @@ import br.com.cmdev.leilao.util.JPAUtil;
 class UsuarioDaoTest {
 
 	private UsuarioDao dao;
-	
 	private EntityManager em;
-	
+
 	@BeforeEach
 	public void setUp() {
 		this.em = JPAUtil.getEntityManager();
+		this.dao = new UsuarioDao(em);
+		em.getTransaction().begin();
+	}
+	
+	@AfterEach
+	public void destroy() {
+		em.getTransaction().rollback();
+	}
+
+	@Test
+	public void deveriaRetornarUmUsuarioPassandoComoParametroOUserName() {
+		Usuario usuario = createUser();
+		assertNotNull(usuario);
 	}
 	
 	@Test
-	void deveriaRetornarUmUsuarioPassandoComoParametroOUserName() {
-		this.dao = new UsuarioDao(em);
-
-		Usuario usuario = new Usuario("fulano", "fulano@email.com", "123456");
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
-		
-		Usuario usuarioEncotrado = this.dao.buscarPorUsername("fulano");
-		assertNotNull(usuarioEncotrado);
-		em.close();
+	public void naoDeveriaRetornarUmUsuarioPassandoComoParametroOUserName() {
+		createUser();
+		assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("baltrano"));
 	}
+	
+	private Usuario createUser() {
+		Usuario usuario = new Usuario("fulano", "fulano@email.com", "123456");
+		em.persist(usuario);
+		return usuario;
+	}
+	
 
 }
